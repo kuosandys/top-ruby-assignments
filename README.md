5b76b86890a7e504de481406b63990240e664c38
Sandy S. Kuo <kuo.sandys@gmail.com>
2020-05-27 15:50:16 +0400

    Created UI for stock_picker.rb in commandline, updated README.md with Stock Picker entry

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. Caesar Cipher (caesar.rb): A caesar cipher that works in the command line by asking users for a message to encode, a shift factor for encoding, and returns the encoded message.
 
 2. Substrings (substrings.rb): Asks the user for some text and checks if the text includes words from our dictionary. Returns to the user a list of all the words found in our dictionary and the number of times it occurs in the text.
+
+3. Stock-Picker (stock_picker.rb): A stock picker that works in the command line. Enter a list of stock-prices and receive advice on which days to buy and sell for maximum profit.
--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,31 @@
-sample_prices = [17,3,6,9,15,8,6,1,10]
-
 def stock_picker(stock_prices)
   profit_hash = {}
   stock_buy = stock_prices[0..-2]
   stock_buy.each_with_index do |buy, buy_index|
     stock_sell = stock_prices[buy_index+1..-1]
     stock_sell.each_with_index do |sell, sell_index|
-      profit_hash[sell - buy] = [buy_index, buy_index+sell_index+1]
+      profit_hash[sell.to_i - buy.to_i] = [buy_index, buy_index+sell_index+1]
     end
   end
-  max_profit_indices = profit_hash.max_by{|profit, prices| profit}[1]
+  max_profit = profit_hash.max_by{|profit, prices| profit}
 end
 
-stock_picker(sample_prices)
+puts "Enter a list of stock prices and I will tell you the best day to buy and sell for maximum profit!"
+puts "Days start at 0"
+puts "Please enter a list of stock prices separated by commas: "
+user_input = gets.chomp.split(",")
+active = true
+
+while active
+  if user_input.kind_of?(Array) && user_input.length > 1
+    best_profit = stock_picker(user_input)
+    puts "The maximum profit is $#{best_profit[0]} - buy on day #{best_profit.last.first} and sell on day #{best_profit.last.last}."
+    active = false
+  else
+    puts "Please enter a valid list of stock prices! Press 'q' to quit."
+    user_input = gets.chomp.split(",")
+      if user_input == 'q'
+        active = false
+      end
+  end
+end
